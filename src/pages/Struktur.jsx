f8c46d87f2e36b5a6bf4541718245126ac4b4e3d
amongkareng <andremongkareng013@gmail.com>
2025-06-30 22:31:27 +0700

    fix asset 2

--- src/pages/Struktur.jsx
+++ src/pages/Struktur.jsx
@@ -1,5 +1,5 @@
 import React, { useEffect } from "react";
-import struktur from "/src/assets/struktur.png";
+import struktur from "../assets/struktur.png";
 import Header from "../components/Header";
 import Footer from "../components/Footer";
 import AOS from "aos";
